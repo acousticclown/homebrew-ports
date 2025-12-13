a77139932c27f0b23e9d343b15988cca56625a85
Shivansh Pratap <shivanshpratapcmci@gmail.com>
2025-12-14 02:08:27 +0530

    Add ports formula

---
+++ Formula/ports.rb
@@ -0,0 +1,17 @@
+class Ports < Formula
+  desc "Beautiful CLI to list, check and kill running ports"
+  homepage "https://github.com/YOUR_USERNAME/ports"
+  url "https://github.com/YOUR_USERNAME/ports/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "cdc1ebe36509e33f384321f4c0eb416c75da30672deaa1de8d808448e1fdcf1b"
+  license "MIT"
+
+  depends_on "go"
+
+  def install
+    system "go", "build", "-o", bin/"ports"
+  end
+
+  test do
+    system "#{bin}/ports"
+  end
+end
