f145b546b2ba9bf987d2ba50898fe49b1b765ac2
Shivansh Pratap <shivanshpratapcmci@gmail.com>
2025-12-14 02:10:20 +0530

    Update homepage and URL to point to the correct repository

--- Formula/ports.rb
+++ Formula/ports.rb
@@ -1,7 +1,7 @@
 class Ports < Formula
   desc "Beautiful CLI to list, check and kill running ports"
-  homepage "https://github.com/YOUR_USERNAME/ports"
-  url "https://github.com/YOUR_USERNAME/ports/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/acousticclown/ports"
+  url "https://github.com/acousticclown/ports/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "cdc1ebe36509e33f384321f4c0eb416c75da30672deaa1de8d808448e1fdcf1b"
   license "MIT"
 
